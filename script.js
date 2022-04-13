f47c56b0e331c687e3a7121858cc040bd2db9cb1
Akash Meher <akashmeherofficial@gmail.com>
2022-04-13 17:30:47 +0530

    a little update with project

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const tipcalculate = () => {
   let amount = document.getElementById("bill-amount").value;
 
   let percentage = document.getElementById("tip-percentage").value;
+  let bill_split = document.getElementById("bill-split").value;
 
   let tip = amount * (percentage / 100);
 
   let totalamount = tip + Number(amount);
+  let perHead = totalamount / bill_split;
 
   document.getElementById("tip-amount").value = tip;
   document.getElementById("total").value = totalamount;
+  document.getElementById("total-split").value = perHead;
 };
